05d35ff60ca71a841f4285c6ef7a937faa59ad75
elxp101t3r <eliotx10101@outlook.com>
2023-11-25 10:56:19 +0200

    refactor complete

--- main.py
+++ main.py
@@ -3,21 +3,14 @@ from config import *
 from snake import Snake
 import time
 
-
 snake = Snake()
-
-
+snake.snake_create()
 
 game_mode = True
 
 while game_mode:
     screen.update()
     time.sleep(0.1)
-    
     snake.move()
     
-        
-
-
-
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,17 +1,21 @@
 from turtle import Turtle
+
+
 class Snake:
-    def move():
-        positions = [(0, 0), (-20, 0), (-40, 0)]
-        blocks = []
-        for p in positions:
+    def __init__(self):
+        self.positions = [(0, 0), (-20, 0), (-40, 0)]
+        self.blocks = []
+    def snake_create(self):
+        for p in self.positions:
             new_block = Turtle()
             new_block.penup()
             new_block.shape('square')
             new_block.color('white')
             new_block.setposition(p)
-            blocks.append(new_block)
-        for block in range(len(blocks) - 1, 0, -1):
-            n_x = blocks[block - 1].xcor()
-            n_y = blocks[block - 1].ycor()
-            blocks[block].goto(n_x, n_y)
-        blocks[0].forward(20)
+            self.blocks.append(new_block)
+    def move(self):
+        for block in range(len(self.blocks) - 1, 0, -1):
+            n_x = self.blocks[block - 1].xcor()
+            n_y = self.blocks[block - 1].ycor()
+            self.blocks[block].goto(n_x, n_y)
+        self.blocks[0].forward(20)
