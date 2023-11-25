e93dc1cfa5ba35dea759ed1b0879dfbfd6759e17
elxp101t3r <eliotx10101@outlook.com>
2023-11-25 11:32:32 +0200

    Move extended with key listeners

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import time
 
 snake = Snake()
 
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+
 game_mode = True
 
 while game_mode:
--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 
 S_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_D = 20
 
 class Snake:
     
@@ -10,7 +11,7 @@ class Snake:
         
         
     def snake_create(self):
-        for p in self.positions:
+        for p in S_POSITIONS:
             new_block = Turtle()
             new_block.penup()
             new_block.shape('square')
@@ -20,8 +21,29 @@ class Snake:
             
             
     def move(self):
+        """Moving the snake"""
         for block in range(len(self.blocks) - 1, 0, -1):
             n_x = self.blocks[block - 1].xcor()
             n_y = self.blocks[block - 1].ycor()
             self.blocks[block].goto(n_x, n_y)
-        self.blocks[0].forward(20)
+        self.blocks[0].forward(MOVE_D)
+    
+    
+    def up(self):
+        """Moving the snake up"""
+        self.blocks[0].setheading(90)
+        
+        
+    def down(self):
+        """Moving the snake down"""
+        self.blocks[0].setheading(270)
+    
+    def left(self):
+        """Moving the snake left"""
+        self.blocks[0].setheading(180)
+       
+    
+    def right(self):
+        """Moving the snake right"""
+        self.blocks[0].setheading(0)
+        
