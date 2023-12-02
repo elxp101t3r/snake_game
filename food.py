1d5b8825ed846dc6815d7d8dee16bcb507057e14
elxp101t3r <eliotx10101@outlook.com>
2023-12-02 20:42:09 +0200

    Fix typo

--- food.py
+++ food.py
@@ -4,7 +4,7 @@ import random as r
 class Food(Turtle):
     
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.shape('circle')
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
