bc7704d5d374a0b9cec94d28c72f8dc962e0ad0e
elxp101t3r <eliotx10101@outlook.com>
2024-01-01 03:27:48 +0200

    Adding a plain text db method for highscore

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class ScoreBoard(Turtle):
         self.penup()
         self.goto(-50, 270)
         self.score = 0
-        self.highscore = 0
+        with open('data.txt', mode='r') as f:
+            self.highscore = int(f.read()) if f.readable() else None
         self.write(f'Score: {self.score} High Score: {self.highscore}', align='center',move=False, font=('Courier', 15, 'normal'))
     
     
@@ -21,6 +22,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open('data.txt', 'w') as f:
+                f.write(str(self.highscore))
         self.score = 0
         self.update_scoreboard()
         
